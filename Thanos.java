0277e95127d41621e44174d265706cf5cbd8faf1
Victor Todeschini <victodeschini@yahoo.fr>
2019-05-27 12:23:05 +0200

    Java collection #1

--- Thanos.java
+++ Thanos.java
@@ -4,25 +4,44 @@ public class Thanos {
 
     public static void main(String[] args) {
 
-        // TODO 1 : Create an empty heroes list
+        Hero blackWidow = new Hero("Black Widow", 34);
+        Hero captainAmerica = new Hero("Captain America", 100);
+        Hero vision = new Hero("Vision", 3);
+        Hero ironMan = new Hero("Iron Man", 48);
+        Hero scarletWitch = new Hero("Scarlet Witch", 29);
+        Hero thor = new Hero("Thor", 1500);
+        Hero spidey = new Hero("Spider-Man", 18);
+        Hero hulk = new Hero("Hulk", 49);
+        Hero strange = new Hero("Doctor Strange", 42);
+        
+        ArrayList<Hero> heroes = new ArrayList<>();
 
-        // TODO 2 : Add those heroes to the list
-        // name: Black Widow, age: 34
-        // name: Captain America, age: 100
-        // name: Vision, age: 3
-        // name: Iron Man, age: 48
-        // name: Scarlet Witch, age: 29
-        // name: Thor, age: 1500
-        // name: Spider-Man, age: 18
-        // name: Hulk, age: 49
-        // name: Doctor Strange, age: 42
+        heroes.add(blackWidow);
+        heroes.add(captainAmerica);
+        heroes.add(vision);
+        heroes.add(ironMan);
+        heroes.add(scarletWitch);
+        heroes.add(thor);
+        heroes.add(spidey);
+        heroes.add(hulk);
+        heroes.add(strange);
 
-        // TODO 3 : It's Thor birthday, now he's 1501
+        thor.setAge(1501);
 
-        // TODO 4 : Shuffle the heroes list
+        Collections.shuffle(heroes);
 
-        // TODO 5 : Keep only the half of the list
+        int maxSize = heroes.size();
 
-        // TODO 6 : Loop throught the list and display the name of the remaining heroes
+        for(int i = heroes.size()-1; i>maxSize/2 -1; i--){
+            heroes.remove(i);
+        }
+
+        for(Hero hero : heroes){
+            System.out.println(hero.getName());
+        }
+
+
+
+        
     }
 }
